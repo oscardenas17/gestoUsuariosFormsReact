d7b2e05a2fad599bd2750b4062123b3f1d360030
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-03-27 16:03:06 -0500

    Optimización de rendimiento

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import { useState } from "react";
-import useFormulario from "./hooks/useFormulario";
-import Input from "./components/Input";
+
 import Card from "./components/Card";
 import Container from "./components/Container";
-import Button from "./components/Button";
+import UserForm from './components/UserForm'
 
 function App() {
-  const [usuarios, setUsuarios] = useState([]);
-  const [formulario, handleChange, reset] = useFormulario({
-    name: "",
-    lastname: "",
-    email: "",
-  });
+  const [usuarios, setUsuarios] = useState([])
 
-  const submit = (e) => {
-    e.preventDefault();
-    setUsuarios([...usuarios, formulario]);
-    reset();
+
+  const submit = (usuario) => {
+  
+    setUsuarios([
+      ...usuarios,
+      usuario
+    ])
+   
   };
 
   //console.log(formulario, usuarios);
@@ -25,33 +23,7 @@ function App() {
       <Container>
         <Card>
           <div style={{ padding: 20 }}>
-            <form onSubmit={submit}>
-              <Input
-                label="Nombre"
-                name="name"
-                value={formulario.name}
-                onChange={handleChange}
-                placeholder='Nombre'
-              />
-
-              <Input
-                label="Apellido"
-                name="lastname"
-                value={formulario.lastname}
-                onChange={handleChange}
-                placeholder='Apellido'
-              />
-
-              <Input
-                label="Correo"
-                name="email"
-                value={formulario.email}
-                onChange={handleChange}
-                placeholder='Correo'
-              />
-
-              <Button>Enviar</Button>
-            </form>
+            <UserForm submit={submit}></UserForm>
           </div>
         </Card>
 
