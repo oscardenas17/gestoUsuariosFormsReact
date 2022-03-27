b681e40aa2f7d6286c3248ae7bd4ccdcea5276ff
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-03-26 23:49:25 -0500

    Componente Container

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-
-import useFormulario from './hooks/useFormulario'
-import Input from './components/Input'
-import Card from './components/Card'
+import useFormulario from "./hooks/useFormulario";
+import Input from "./components/Input";
+import Card from "./components/Card";
+import Container from "./components/Container";
 
 function App() {
- const [formulario, handleChange] = useFormulario({name: '', lastname: ''})
+  const [formulario, handleChange] = useFormulario({ name: "", lastname: "" });
 
-  console.log(formulario)
+  console.log(formulario);
   return (
-  <Card>
-     <form>
-      <Input 
-        label="Nombre" 
-        name='name' 
-        value={formulario.name} onChange={handleChange} 
-      />
+    <Container>
+      <Card>
+        <div style={ {padding:20}}>
+          <form>
+            <Input
+              label="Nombre"
+              name="name"
+              value={formulario.name}
+              onChange={handleChange}
+            />
 
-      <Input 
-        label="Apellido" 
-        name='lastname' 
-        value={formulario.lastname} 
-        onChange={handleChange}
-       />
-   </form>
-  </Card>
-  )
+            <Input
+              label="Apellido"
+              name="lastname"
+              value={formulario.lastname}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+      </Card>
+    </Container>
+  );
 }
 
-export default App
+export default App;
